869ffb5a8ab2c92149d60719b1bfe801d79e4972
panuganti <rajkiran.panuganti@gmail.com>
2017-02-09 22:23:19 -0800

    Setup Login Page; Add Tweet Component; Add Library

---
+++ src/pages/login/login.ts
@@ -0,0 +1,41 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+import { AngularFire, AuthMethods, AuthProviders } from 'angularfire2';
+import { HomePage } from '../home/home';
+//import * as firebase from 'firebase';
+
+@Component({
+  selector: 'page-login',
+  templateUrl: 'login.html'
+})
+export class LoginPage {
+
+  constructor(public navCtrl: NavController, public af: AngularFire) {
+  }
+
+  loginTwitter() {
+    let self = this;
+    this.af.auth.login({
+      provider: AuthProviders.Twitter,
+      method: AuthMethods.Popup
+    }).then(function (response) {
+      console.log(response);
+      self.navCtrl.setRoot(HomePage);
+    }).catch(function (error) {
+      console.log(error);
+    });
+  }
+
+  loginFb() {
+    let self = this;
+    this.af.auth.login({
+      provider: AuthProviders.Facebook,
+      method: AuthMethods.Popup
+    }).then(function (response) {
+      self.navCtrl.setRoot(HomePage);
+    }).catch(function (error) {
+      console.log(error);
+    });
+  }
+
+}
