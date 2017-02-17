112da1effab99f2adb2ac7564f587a4469213626
rapanuga <rapanuga@microsoft.com>
2017-02-16 16:11:00 -0800

    changes to search page

--- src/pages/search/search.ts
+++ src/pages/search/search.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams, ViewController } from 'ionic-angular';
 import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import * as firebase from 'firebase';
 
 @Component({
   selector: 'page-search',
@@ -12,23 +13,49 @@ export class SearchPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams,
     public viewCtrl: ViewController, public af: AngularFire) {
-      this.celebs = this.af.database.list('/Celebs');
-     }
+    this.celebs = this.af.database.list('/Celebs');
+  }
 
   dismiss() {
     this.viewCtrl.dismiss();
   }
 
-  getCelebs(celebs: any) {
+  onInput(ev: any) {
+    this.input = ev.target.value;
+  }
+
+  getCelebs(celebs: any, input: string) {
+    if (input == null || input.length == 0) { return null; }
+    let self = this;
     if (this.input && this.input.trim() != '') {
       celebs = celebs.filter((item) => {
-        return (item.name.toLowerCase().indexOf(this.input.toLowerCase()) > -1);
+        return (item.name.toLowerCase().indexOf(self.input) > -1);
       })
     }
     return celebs;
   }
 
-  addToWatchlist(celeb) {
+  onCancel(ev: any) {
+    this.input = '';
+  }
+
+  addToWatchlist(id) {
+    let user = firebase.auth().currentUser;
+    let parentThis = this;
+    this.af.database.object('/Users/' + user.uid)
+      .subscribe((obj) => {
+        if (!obj.$exists()) {
+          try {
+            parentThis.af.database.list('/Watchlists/' + user.uid).push({ id: id });
+          }
+          catch (error) {
+            console.log(error);
+          }
+        }
+        else {
+          parentThis.af.database.list('/Watchlists/' + user.uid).push({ id: id });
+        }
+      })
     this.viewCtrl.dismiss();
   }
 
